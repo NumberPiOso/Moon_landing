dc6bcf7d09f40263a0cf3118182f11913dfa8509
Pi <osoriopabl@gmail.com>
2020-02-26 21:44:05 -0500

    Now the initial population (best of initial games) is stored

--- code/main.py
+++ code/main.py
@@ -11,7 +11,7 @@ LR = 1e-3
 env = gym.make('LunarLander-v2')
 env.reset()
 goal_steps = 200
-score_requirement = 50
+score_requirement = 0
 initial_games = 1000
 
 def some_random_games_first():
@@ -25,5 +25,44 @@ def some_random_games_first():
             if done:
                 break
 
+def initial_population():
+    training_data = []
+    scores = []
+    accepted_scores = []
+    for _ in range(initial_games):
+        score = 0
+        game_memory = []
+        prev_observation = []
+        for _ in range(goal_steps):
+            action = random.randrange(0, 4)
+            observation, reward, done, info = env.step(action)
+            if len(prev_observation) > 0:
+                game_memory.append([prev_observation, action])
+            
+            prev_observation = observation
+            score += reward
+            if done:
+                break
 
-some_random_games_first()
+        if score > score_requirement:
+            accepted_scores.append(score)
+            for data in game_memory:
+                output = [0,0,0,0]
+                output[data[1]] = 1
+                training_data.append([data[0], output])
+
+        env.reset()
+        scores.append(score)
+    training_data_save = np.array(data)
+    np.save('saved.npy', training_data_save)
+
+    print('Average accepted score', np.mean(accepted_scores))
+    print('Median accepted score', np.median(accepted_scores))
+    print(accepted_scores)
+
+    print('Mean all scores ', np.mean(scores))
+    print('Median all score', np.median(scores))
+
+
+# some_random_games_first()
+initial_population()
