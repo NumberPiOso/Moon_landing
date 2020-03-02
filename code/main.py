0bd4feb8292611abd72a2e438fe0b2fe60ecb4a3
Pi <osoriopabl@gmail.com>
2020-03-01 21:56:27 -0500

    creating the neural network per se

--- code/main.py
+++ code/main.py
@@ -1,10 +1,12 @@
 import gym
 import random
 import numpy as np
-import tensorflow
-# import tflearn
-# from tflearn.layers.core import input_data, dropout, fully_connected
-# from tflearn.layers.estimator import regression
+import tensorflow as tf
+# from tensorflow import keras
+# from tensorflow.keras import layers
+import tflearn
+from tflearn.layers.core import input_data, dropout, fully_connected
+from tflearn.layers.estimator import regression
 from collections import Counter
 
 LR = 1e-3
@@ -58,11 +60,45 @@ def initial_population():
 
     print('Average accepted score', np.mean(accepted_scores))
     print('Median accepted score', np.median(accepted_scores))
-    print(accepted_scores)
+    print(len(accepted_scores))
 
     print('Mean all scores ', np.mean(scores))
     print('Median all score', np.median(scores))
 
+def neural_network_model(input_size):
+    network = input_data(shape=[None, input_size, 1], name='input')
 
+    network = fully_connected(network, 64, activation='relu')
+    network = dropout(network, .8)
+
+    network = fully_connected(network, 128, activation='relu')
+    network = dropout(network, .8)
+
+    network = fully_connected(network, 256, activation='relu')
+    network = dropout(network, .8)
+
+    network = fully_connected(network, 128, activation='relu')
+    network = dropout(network, .8)
+
+    network = fully_connected(network, 64, activation='relu')
+    network = dropout(network, .8)
+
+    network = fully_connected(network, 2, activation='softmax')
+    network = regression(network, optimizer='adam', learning_rate=LR,
+                        loss='categorical_crossentropy', name='tragets')
+    model = tflearn.DNN(network, tensorboard_dir='log')
+
+    return model
+    
+
+def train_model(training_data, model=False):
+    X = np.array([i[0] for i in training_data]).reshape(-1, len(training_data[0][0]))
+    y = [i[1] for i in training_data]
+
+    if not model:
+        model = neural_network_model(input_size = len(X[0]))
+    model.fit({'input' :X}, {'targets' :y}, n_epochs=5, snapshot_step=500,
+            show_metric=True, run_id='OpenAIgymProject')
 # some_random_games_first()
-initial_population()
+# initial_population()
+training_data = initial_population()
